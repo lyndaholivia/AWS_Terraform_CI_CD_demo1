ec7aa4dfdc7bcefa97bc212bcd7a00962de97b69
lyndaholivia <lynda.holivia@gmail.com>
2025-08-22 13:18:50 +0100

    changes to main

--- src/main.tf
+++ src/main.tf
@@ -1,22 +1,22 @@
 terraform {
-    backend "s3" {
-        bucket         = "cc-tf-state-backend-ci-cd-lynda1"
-        key            = "tf-infra/terraform.tfstate"
-        region         = "us-east-1"
-        dynamodb_table = "terraform_state-locking1"
-        encrypt        = true
+  backend "s3" {
+    bucket         = "cc-tf-state-backend-ci-cd-lynda1"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_state-locking1"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
-    required_version = ">=0.13.0"
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.0"
-        }
-    } 
+  }
 }
 provider "aws" {
-        region = "us-east-1"
-    }
+  region = "us-east-1"
+}
 
 module "tf_state" {
   source      = "./modules/tf-state"
